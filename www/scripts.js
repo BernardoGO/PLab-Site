3ee5ad422ea506c26205012b960b4df548ed991d
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-05-25 16:07:13 -0300

    js

--- www/scripts.js
+++ www/scripts.js
@@ -2,5 +2,6 @@ function point_it(event){
 	alert(event);
 	var milliseconds = (new Date).getTime();
 	//window.location.href = "http://[IP]:[PORT]/index.html?mouse_x="+pos_x+"&mouse_y="+pos_y+ "&epoch="+milliseconds;
+    window.location.href = "commd.py?cmd=" + event;
 }
 
